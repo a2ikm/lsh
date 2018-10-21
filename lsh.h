f9e0c5d29df976f6f15eea5c52e863d15e5131a0
Masato Ikeda <masato.ikeda@gmail.com>
2018-10-22 00:58:48 +0900

    Rename builtin.h to lsh.h

--- lsh.h
+++ lsh.h
@@ -1,2 +1,7 @@
+#ifndef LSH_H
+#define LSH_H
+
 typedef int (*lsh_builtin_func) (char **);
 lsh_builtin_func lsh_find_builtin_func(char *name);
+
+#endif
--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "builtin.h"
+#include "lsh.h"
 
 int lsh_launch(char **args)
 {
